3a52998840ef41df2e6de464f0785f7351aeb759
Sam Grantham <sagran@microsoft.com>
2025-07-07 16:27:50 -0500

    Refactor to allow for threaded approach to running the Api calls.

--- PermissionHelper.ps1
+++ PermissionHelper.ps1
@@ -1,16 +1,24 @@
 
-function Get-MSALToken 
-{
-    try
+function Get-MSALToken {
+    if (Get-Module -Name Az -ListAvailable)
     {
-        Write-Host "AZ login will open a modal identity picker in the top left of the screen, please choose the account you want to use."
-        Write-Host "It may take a few seconds to load, please be patient."  
-        Write-Host "It will then ask you to pick a subscription, please choose a subscription that is associated with the tenant that backs your org."
-        Connect-AzAccount 
-        $result = Get-AzAccessToken -ResourceUrl '499b84ac-1321-427f-aa17-267ca6975798'
-        Clear-Host
+        try
+        {
+            Write-Host "Found Az module, attempting to use it to get an access token."
+            $result = Get-AzAccessToken -ResourceUrl '499b84ac-1321-427f-aa17-267ca6975798'
+            Clear-Host
+        }
+        catch
+        {
+            Write-Host "AZ login will open a modal identity picker in the top left of the screen, please choose the account you want to use."
+            Write-Host "It may take a few seconds to load, please be patient."  
+            Write-Host "It will then ask you to pick a subscription, please choose a subscription that is associated with the tenant that backs your org."
+            Connect-AzAccount 
+            $result = Get-AzAccessToken -ResourceUrl '499b84ac-1321-427f-aa17-267ca6975798'
+            Clear-Host
+        } 
     }
-    catch
+    else
     {
         Clear-Host
         Write-Host "It seems that the Az module is not installed or not working properly."
@@ -34,6 +42,8 @@ function GET-AzureDevOpsRestAPI {
         [string]$RestAPIUrl
     )
     #Write-Host "Calling Azure DevOps Rest API"
+    #Write-Host "URL: $RestAPIUrl"
+    #Write-Host "AuthHeader: $Authheader"
     #$debug = $false
     $Headers = @{
         Authorization           = $Authheader
@@ -101,13 +111,14 @@ function GET-AzureDevOpsRestAPI {
 }#>
 }
 
-# PowerShell doesn't have a Main function like C# or Java, so we can just call the functions directly.
-    $orgname = "sagranvso"
-    $orgUrl = "https://dev.azure.com/$orgname"
-    $token = Get-MSALToken
-    $plainToken = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($token.Token))
-    $Authheader = "Bearer $plaintoken"
-    Write-Host "Token acquired for User: $($token.UserId) in TenantID: $($token.TenantId). Good until $($token.ExpiresOn)"     
+function Get-AzureDevOpsPermissions {
+    param (
+        [string]$Authheader,
+        [string]$orgUrl
+    )
+    # This function retrieves the permissions for all namespaces in the Azure DevOps organization.
+    # It uses the MSAL token to authenticate and make REST API calls to Azure DevOps.
+    # The results are returned as a collection of permission items.
     $namespaceUrl = "$($orgUrl)/_apis/securitynamespaces?api-version=7.2-preview.1"
     $namespaces = GET-AzureDevOpsRestAPI -RestAPIUrl $namespaceUrl -Authheader $Authheader
     $queue = @()
@@ -117,12 +128,9 @@ function GET-AzureDevOpsRestAPI {
         $permissionResult = GET-AzureDevOpsRestAPI -RestAPIUrl $permissionUrl -Authheader $Authheader
         foreach ($permission in $permissionResult.results.value)
         {
-            #((($permission.acesDictionary).psobject.Properties).Value)
             foreach ($descriptor in ((($permission.acesDictionary).psobject.Properties).Value))
             {  
                 $_descriptor = $descriptor.descriptor
-                #Write-Host "Processing Namespace: $($namespace.namespaceId) - $($namespace.displayName) - Token: $($permission.token)"
-                #Write-Host "Descriptor: $($_descriptor)"
                 $allowNullSafe                = ($null -eq $permission.acesDictionary."$_descriptor".allow) ? 0 : $permission.acesDictionary."$_descriptor".allow
                 $denyNullSafe                 = ($null -eq $permission.acesDictionary."$_descriptor".deny) ? 0 : $permission.acesDictionary."$_descriptor".deny
                 $effectiveAllowNullSafe       = ($null -eq $permission.acesDictionary."$_descriptor".extendedInfo.effectiveAllow) ? 0 : $permission.acesDictionary."$_descriptor".extendedInfo.effectiveAllow
@@ -171,9 +179,53 @@ function GET-AzureDevOpsRestAPI {
                 if ($permissionitem.allow -gt 0 -or $permissionitem.deny -gt 0 -or $permissionitem.effectiveAllow -gt 0 -or $permissionitem.effectiveDeny -gt 0)
                 {
                     $queue += $permissionitem
-                    #Write-Host "Namespace: $($namespace.namespaceId) - $($namespace.displayName) - Token: $($permissionitem.friendlyToken) - $enumactions"
                 }
             }
         }
     }
-    $queue | Export-Csv -Path ".\Permissions.csv" -NoTypeInformation -Force
+#    $queue | Export-Csv -Path ".\Permissions.csv" -NoTypeInformation -Force
+    $queue | ConvertTo-Json -Depth 100 | Out-File -FilePath ".\Permissions.json" -Force
+}
+
+function Main {
+    Write-Host "Please enter your Org Name"
+    $orgName = Read-Host
+    $orgUrl = "https://dev.azure.com/$orgname"
+    $token = Get-MSALToken
+    $plainToken = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($token.Token))
+    $Authheader = "Bearer $plaintoken"
+    Write-Host "Token acquired for User: $($token.UserId) in TenantID: $($token.TenantId). Good until $($token.ExpiresOn)"     
+    Write-Host "Retrieving Azure DevOps permissions for Org: $orgName"
+
+    $scriptPath = $MyInvocation.MyCommand.Path
+    if (-not $scriptPath) {
+        Write-Host "Script path not found. Using current directory."
+        $scriptPath = Get-ChildItem -Path "$((Get-Location).Path)\PermissionHelper.ps1" -ErrorAction Stop | Select-Object -First 1
+    }
+    Write-Host "Script path: $scriptPath"
+    $job = Start-ThreadJob -ScriptBlock {
+        param($Authheader, $orgUrl, $scriptPath)
+        $env:IS_CHILD_JOB = $true
+        . "$scriptPath"
+        Get-AzureDevOpsPermissions -Authheader $Authheader -orgUrl $orgUrl
+    } -ArgumentList $Authheader, $orgUrl, $scriptPath
+    # We should be able to continue to start functions on threads here
+    # We will need to Gather things like Identities, Groups, Teams, and Projects
+    # I plan to save each result set to a file, and then combine them at the end.
+    # this seems to be the best way to do this, as it allows us to run multiple jobs in parallel.
+    # and also for large orgs, not overwhelm system memory trying to hold all the results in memory at once.
+    # we will also need to handle throttling, as we will be making a lot of heavy API calls.
+    # we can watch for the Retry-After header in the response, and if it is present, we can wait that many seconds before retrying the request.
+    # We can also use the X-RateLimit-Remaining header to determine how many requests we have left before we hit the rate limit.
+    # If we hit the rate limit, we can wait for the X-RateLimit-Reset header to determine when we can start making requests again.
+    # We can also use the X-RateLimit-Delay header to determine how long we need to wait before making the next request
+    Write-Host "Waiting for job to complete..."
+    Wait-Job -Job $job -Timeout 300
+    Write-Host "Job completed."
+    Receive-Job -Job $job
+}
+
+# Only run Main if not running as a job (i.e., if $env:IS_CHILD_JOB is not set)
+if (-not $env:IS_CHILD_JOB) {
+    Main
+}
